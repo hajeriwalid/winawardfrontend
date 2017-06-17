78f7ffa91cba68413286837b67e648ade9fe8b6e
hajeriwalid <walidoo@gmail.com>
2017-06-18 02:24:30 +0900

    basic list - UI only

--- www/components/myscripts.js
+++ www/components/myscripts.js
@@ -1,40 +1,48 @@
-// This is a JavaScript file
 
- 
-        //on page1 init
 document.addEventListener('init', function(event) {
-  if (event.target.matches('#page1')) {
+ 
+ 
+ var page = event.target; 
+ 
+ if (page.matches('#page1')) {
     ons.notification.alert('Page 1 is initiated.');
     // Set up content...
   }
-}, false);
-         
-        
-document.addEventListener('init', function(event) {
-  var page = event.target; 
-
+  
+ 
   if (page.id === 'page1') {
     
     var myNavigator = document.querySelector("#myNavigator");
     
-     myNavigator.addEventListener('postpush', function(event) {
-        ons.notification.alert('This function is called after a new page is pushed.');
-      });
     
     page.querySelector('#push-button').onclick = function() {
       myNavigator.pushPage('page2.html', {data: {title: 'Page 2'}});
     };
+  
+  }
+  
+  
+  
+
     
    
     
-  } else if (page.id === 'page2') {
+   else if (page.id === 'page2') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    
+    var divGD = ons.GestureDetector(document.querySelector('#mylist'));
+    divGD.on('swipeleft swiperight', function(event) {
+     ons.notification.toast('cool');
+    
+    });
+
   }
-});
+  
+  
+}, false);
+         
 
 
-       // or use getElementById("my-navigator")
-     // Add event listener
-      
+
   
  
